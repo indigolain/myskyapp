6afa4b6ccce40ff244107a289ed5bd2f83986226
Takuya Uehara <indigo.lain@gmail.com>
2013-10-02 15:08:37 +0900

    First commit

---
+++ mysky/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mysky::Application.config.secret_key_base = 'eba3005eddaf01bf5e1ac7e16815b37a92339d02065c1a78619e168f70ddbbbabc76c413481f9e520918a9006ab304cbd99327ad0fde3b136593f6b797a32b92'
